d8f512818bbd46c3aaddebe2556be1bf65432c17
Rahul Kuruvilla <32335770+rahulkuruvilla@users.noreply.github.com>
2021-02-14 11:45:09 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("Sunny")
+    music.playMelody("F G A B C5 - C5 - ", 120)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
@@ -7,6 +8,7 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     temp_reading = pins.analogReadPin(AnalogPin.P1)
     temperature = temp_reading * A / B + C
+    temperature += -18
     basic.showNumber(temperature)
 })
 let temperature = 0
@@ -18,5 +20,5 @@ A = 18
 B = 115
 C = -54
 basic.forever(function () {
-	
+    led.setBrightness(150 - input.lightLevel())
 })
